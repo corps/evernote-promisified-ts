6dcb58a5c683db80ac179b1bb0dee53decac9f64
Zach Collins <recursive.cookie.jar@gmail.com>
2015-11-30 19:26:44 -0800

    use 'lie' and assume less of promise

--- test/integration.test.ts
+++ test/integration.test.ts
@@ -1,62 +1,62 @@
-import Promise = require("bluebird");
+require("lie/polyfill");
 import readline = require("readline");
 import assert = require("assert");
 import { Evernote } from "evernote";
-import { setPromiseEngine, UserStore, NoteStore, promisifyUserStore, promisifyNoteStore } from "../index";
-
-setPromiseEngine(Promise);
+import { UserStore, NoteStore, promisifyUserStore, promisifyNoteStore } from "../index";
 
 var readlineInterface = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
+  input: process.stdin,
+  output: process.stdout
 });
 
+declare var Promise:PromiseConstructorLike;
+
 var question = (q:string) => {
-    var deferred = Promise.defer<string>();
+  return new Promise<string>((resolve, reject) => {
     readlineInterface.question(q, (answer:string) => {
-        deferred.resolve(answer);
+      resolve(answer);
     })
-    return deferred.promise;
-}
+  })
+};
 
 describe("integration", function () {
-    this.timeout(60000);
+  this.timeout(60000);
 
-    var userStore:UserStore;
-    var noteStore:NoteStore;
+  var userStore:UserStore;
+  var noteStore:NoteStore;
 
-    before(() => {
-        return question("Please provide a valid evernote auth token: ").then(answer => {
-            var client = new Evernote.Client({token: answer, sandbox: false});
-            userStore = promisifyUserStore(client.getUserStore());
-            noteStore = promisifyNoteStore(client.getNoteStore());
-            return userStore.getUser();
-        }).then(user => {
-            console.log("running tests as ", user.username);
-        });
+  before(() => {
+    return question("Please provide a valid evernote auth token: ").then(answer => {
+      var client = new Evernote.Client({token: answer, sandbox: false});
+      userStore = promisifyUserStore(client.getUserStore());
+      noteStore = promisifyNoteStore(client.getNoteStore());
+      return userStore.getUser();
+    }).then(user => {
+      console.log("running tests as ", user.username);
     });
+  });
 
-    it("can correctly call an api and provide a promise to its result", () => {
-        var filter = new Evernote.NoteFilter();
-        filter.order = Evernote.NoteSortOrder.CREATED;
-        var resultSpec = new Evernote.NotesMetadataResultSpec();
-        resultSpec.includeTitle = true;
+  it("can correctly call an api and provide a promise to its result", () => {
+    var filter = new Evernote.NoteFilter();
+    filter.order = Evernote.NoteSortOrder.CREATED;
+    var resultSpec = new Evernote.NotesMetadataResultSpec();
+    resultSpec.includeTitle = true;
 
-        return noteStore.findNotesMetadata(filter, 10, 5, resultSpec).then(result => {
-            console.log("Found", result.totalNotes, "total notes");
-            console.log("Start index was", result.startIndex);
-            console.log("Note titles were", result.notes.map(n => n.title).join(", "));
-        })
-    });
-
-    context("when an api call fails", () => {
-        it("correctly fails the promise with the expected error", () => {
-            return noteStore.getNote("not-valid-uuid", false, false, false, false).then(() => {
-                assert.ok(false, "Did not expect to succeed!")
-            }, (e:Evernote.EDAMUserException) => {
-                assert.ok(e instanceof Evernote.EDAMUserException);
-                console.log("error code was", e.errorCode, "parameter that was problem:", e.parameter);
-            })
-        })
+    return noteStore.findNotesMetadata(filter, 10, 5, resultSpec).then(result => {
+      console.log("Found", result.totalNotes, "total notes");
+      console.log("Start index was", result.startIndex);
+      console.log("Note titles were", result.notes.map(n => n.title).join(", "));
     })
+  });
+
+  context("when an api call fails", () => {
+    it("correctly fails the promise with the expected error", () => {
+      return noteStore.getNote("not-valid-uuid", false, false, false, false).then(() => {
+        assert.ok(false, "Did not expect to succeed!")
+      }, (e:Evernote.EDAMUserException) => {
+        assert.ok(e instanceof Evernote.EDAMUserException);
+        console.log("error code was", e.errorCode, "parameter that was problem:", e.parameter);
+      })
+    })
+  })
 });
--- typings/tsd.d.ts
+++ typings/tsd.d.ts
@@ -1,4 +1,2 @@
-
 /// <reference path="node/node.d.ts" />
-/// <reference path="bluebird/bluebird.d.ts" />
 /// <reference path="mocha/mocha.d.ts" />
